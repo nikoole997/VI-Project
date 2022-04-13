24566d1fe0234a766472e782f94140e563683231
yashes20 <74877135+yashes20@users.noreply.github.com>
2022-04-13 10:36:13 +0100

    gráfico customBarChat

--- src/App.js
+++ src/App.js
@@ -12,21 +12,36 @@ const App = () => {
   };
 
   getRecords();
+  
   return (
     <>
       <div className="App">
         <header className="App-header">Dashboard</header>
       </div>
-      <div class="box1">
+      <div className="box1">
         <CustomLineChart
           records={records}
           xAxis="LotArea"
           lineKey="SalePrice"
         />
-        <CustomBarChart />
+        
+        <CustomBarChart
+           records={getG(records)} 
+           lineKey="SalePrice"
+        /> 
       </div>
     </>
   );
 };
+var result = [];
+const getG = (array) => Object.values(array.reduce((acc,value)=>{
+  if (!acc[value.YrSold] ){
+    acc[value.YrSold] = { YrSold: value.YrSold, SalePrice: 0 };
+    result.push(acc[value.YrSold]);
+  }
+  acc[value.YrSold].SalePrice += value.SalePrice;
+  return acc;
+
+ },{}));
 
 export default App;
--- src/charts/bar/CustomBarChart.js
+++ src/charts/bar/CustomBarChart.js
@@ -59,21 +59,20 @@ const CustomBarChart = (props) => {
     <BarChart
       width={700}
       height={500}
-      data={data}
+      data={props.records} 
       margin={{
         top: 5,
         right: 30,
         left: 20,
         bottom: 5,
       }}
-    >
+    > 
       <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
+      <XAxis dataKey="YrSold"/>
+      <YAxis dataKey="SalePrice"/>
       <Tooltip />
       <Legend />
-      <Bar dataKey="pv" fill="#8884d8" />
-      <Bar dataKey="uv" fill="#82ca9d" />
+      <Bar  type="monotone" dataKey={props.lineKey} stroke="#8884d8" />
     </BarChart>
   );
 };
